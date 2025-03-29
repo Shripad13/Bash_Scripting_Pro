3e55896039a72ed9884a3b061d2a28f768460f00
Shripad Maddi <Shripad.Maddi@accelya.com>
2025-03-29 21:00:00 +0530

    Bash_scripting

---
+++ Bash_Scripting/01-shebang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello Welcome to Bash Scripting"
+sleep 2
+
+echo "I will learn bash Scripting asap"
+
+sleep 2
+
+echo "script name: $# completed"
---
+++ Bash_Scripting/02-print.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "Hello! \nWelcome Shripad"
+
+echo -e "\t ** Hope you will learn with full focus **"
